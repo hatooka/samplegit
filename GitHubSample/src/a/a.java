f203f0db6898eb5ef10ec262acbffab035a00d77
エキサイト鳩岡 <@LAPTOP-TG655N3I>
2019-06-13 17:10:34 +0900

    練習

--- GitHubSample/src/a/a.java
+++ GitHubSample/src/a/a.java
@@ -10,7 +10,7 @@ import javax.servlet.http.HttpServletResponse;
 /**
  * Servlet implementation class a
  */
-@WebServlet("/a")
+@WebServlet("/SampleServlet")
 public class a extends HttpServlet {
 	private static final long serialVersionUID = 1L;
        
